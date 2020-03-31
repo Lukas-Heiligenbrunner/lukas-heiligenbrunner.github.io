2966ca55baea70321edef1d6e7ba20b5ce8d6cba
Lukas Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2020-03-31 22:52:31 +0200

    animations also in contact and experience section

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(function () {
+    ;(function ($, win) {
+        $.fn.inViewport = function (cb) {
+            return this.each(function (i, el) {
+                function visPx() {
+                    const H = $(this).height(),
+                        r = el.getBoundingClientRect(), t = r.top, b = r.bottom;
+                    return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
+                }
+
+                visPx();
+                $(win).on("resize scroll", visPx);
+            });
+        };
+    }(jQuery, window));
+
+
+    $("#section3").inViewport(function (px) {
+        if (px) {
+            $("#experienceheader").addClass("animationappear600ms");
+            $("#experiencecontentcontainer").addClass("animationappear300ms");
+        }
+    });
+
+    $("#section5").inViewport(function (px) {
+        if (px) {
+            $("#contactheader").addClass("animationappear600ms");
+            $("#contactsubheader").addClass("animationappear300ms");
+            $(".personalcontacts").addClass("animationappearnow");
+        }
+    });
+})
